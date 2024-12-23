43f2000d52ae97cf9360c7bf33a3f125fc3cf278
Nayan Kumar <naayaankumar@gmail.com>
2024-12-23 16:59:24 +0530

    solved the singleFile error

--- src/components/SingleTodoPage.tsx
+++ src/components/SingleTodoPage.tsx
@@ -1,10 +1,18 @@
+
 import {useEffect, useState} from "react";
 import axios from "axios";
 import {useParams} from "react-router-dom";
 
+interface Todo {
+    id: number;
+    userId: number;
+    title: string;
+    completed: boolean;
+}
+
 export default function SingleTodoPage(){
     const {id} = useParams();
-    const [todoData, setTodoData] = useState({})
+    const [todoData, setTodoData] = useState<Todo | null>(null)
 
     useEffect(()=>{
         const getTodos = async ()=>{
@@ -21,10 +29,10 @@ export default function SingleTodoPage(){
 
 return <div key={id}>
 
-    <div>Id: {todoData.id}</div>
-    <div>UserId: {todoData.userId}</div>
-    <div>Title: {todoData.title}</div>
-    <div>Completed: {todoData.completed ? 'True' :"False"}</div>
+    <div>Id: {todoData?.id ?? "N/A"}</div>
+    <div>UserId: {todoData?.userId ?? "N/A"}</div>
+    <div>Title: {todoData?.title ?? "N/A"}</div>
+    <div>Completed: {todoData?.completed ? "True" : "False"}</div>
 
 
 </div>
